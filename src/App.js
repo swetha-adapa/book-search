83a36b12acfc66a9665f7dd502743a320cddc7f6
swetha-adapa <102689858+swetha-adapa@users.noreply.github.com>
2022-12-28 13:24:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+
+import react ,{useState}from "react";
+import Card from "./Card";
+import axios from "axios";
+import './App.css';
+const App=()=>{
+    const [search,setSearch]=useState("");
+    const [bookData,setData]=useState([]);
+    const searchBook=(evt)=>{
+        if(evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+            .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    }
+    return(
+        <>
+            <div className="header">
+                
+                <div className="row2">
+                    <h2>BOOK SEARCH</h2>
+                    <div className="search">
+                        <input type="text" placeholder="Enter Your Book Name"
+                        value={search} onChange={e=>setSearch(e.target.value)}
+                        onKeyPress={searchBook}/>
+                        <button><i className="fas fa-search"></i></button>
+                    </div>
+                    <img src="./images/bg2.png" alt="" />
+                </div>
+            </div>
+
+            <div className="container">
+              {
+                    <Card book={bookData}/>
+              }  
+            </div>
+        </>
+    )
+}
+export default App;
